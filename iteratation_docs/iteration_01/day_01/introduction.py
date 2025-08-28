373b265384fd5bb1ef51c29b37e2eaba1356b05e
KSantos1995 <ksantos@nmhschool.org>
2025-08-28 08:17:30 -0400

    Delete iteratation_docs directory
    Removing iteratation typo directory

--- iteratation_docs/iteration_01/day_01/introduction.py
+++
@@ -1,9 +0,0 @@
-# This is an introductory assignment to help you get on your feet with variables and print statements.
-
-full_name = "Kevin Santos"
-year_of_graduation = 2014
-hobbies = ["Soccer", "Snowboarding", "Hiking"]
-programming_experience = "I studied computer science at Skidmore College. I went on to work at a non-profit educational technology company for 4 years doing software development. Mostly modeling and front end development."
-years_at_nmh = 2
-
-print(f"Hello, my name is {full_name}. I am in the class of {year_of_graduation} and I have been at NMH for {years_at_nmh} years. Some of my hobbies are {hobbies}. In terms of programming experience, {programming_experience}")
